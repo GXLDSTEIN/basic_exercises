9b3e22823bc0bda4e7961b492130f73bad491bbd
gxldstein <gxldstein@tutanota.com>
2023-12-05 09:02:39 +0000

    refactor: new gender_count() parameter

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -113,9 +113,7 @@ def check_gender(name: str) -> bool:
     return is_male[name]
 
 
-def gender_count(
-    school_class: list[dict], female_only: bool = False, male_only: bool = False
-):
+def gender_count(school_class: list[dict], gender: str = None):
     male = 0
     female = 0
     for student in school_class["students"]:
@@ -123,9 +121,9 @@ def gender_count(
             male += 1
         else:
             female += 1
-    if female_only:
+    if gender == "female":
         return female
-    elif male_only:
+    elif gender == "male":
         return male
     else:
         return f'{school_class["class"]}: {female=}, {male=}'
@@ -150,14 +148,13 @@ is_male = {
     "Олег": True,
     "Миша": True,
 }
-female_max = 0
-male_max = 0
+female_max, male_max = 0, 0
 for school_class in school:
-    if gender_count(school_class, female_only=True) > female_max:
-        female_max = gender_count(school_class, female_only=True)
+    if gender_count(school_class, gender="female") > female_max:
+        female_max = gender_count(school_class, gender="female")
         female_max_class = school_class["class"]
-    if gender_count(school_class, male_only=True) > male_max:
-        male_max = gender_count(school_class, male_only=True)
+    if gender_count(school_class, gender="male") > male_max:
+        male_max = gender_count(school_class, gender="male")
         male_max_class = school_class["class"]
 print(f"the most girls ({female_max}) are in class {female_max_class}")
 print(f"the most boys ({male_max}) are in class {male_max_class}")
